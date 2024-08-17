937dbd7ecdadc57eac69779081ae0a3a533b985e
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-08-17 12:14:54 +0530

    Finish jetpack compose tutorial app

--- assignments/assignment-02/app/src/main/java/com/compose/article/MainActivity.kt
+++ assignments/assignment-02/app/src/main/java/com/compose/article/MainActivity.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.compose.article.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,6 +31,7 @@ class MainActivity : ComponentActivity() {
             ComposeArticleTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize()
+                        .statusBarsPadding()
                 ) {
                     ComposeArticleApp(
                         modifier = Modifier.fillMaxSize()
@@ -34,10 +44,33 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ComposeArticleApp(modifier: Modifier = Modifier) {
-
+    Column {
+        Image(
+            painter = painterResource(id = R.drawable.bg_compose_background),
+            contentDescription = "Hello World"
+        )
+        Text(
+            text = stringResource(R.string.jetpack_compose_tutorial),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = stringResource(R.string.two),
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = stringResource(R.string.three),
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
-@Preview
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
 @Composable
 fun ComposeArticleAppPreview() {
     ComposeArticleApp(
